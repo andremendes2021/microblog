b1e9806ae9384a8bf2112641e39c49daeda09f53
andremendes2021 <82843262+andremendes2021@users.noreply.github.com>
2024-04-23 17:03:45 -0300

    Passagens de parâmetros.

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,28 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, request
 from app import app
 
 @app.route('/')
-@app.route('/index')
-def index():
-    nome = "Nabu"
-    dados = {"profissão": "Aspone", "canal": "Aspone Nabu"}
+@app.route('/index', defaults={"nome":"Usuário", "profissao":"Aux. de Aspone", "canal":"Canauxaspone"})
+    
+@app.route('/index/<nome>/<profissao>/<canal>')
+def index(nome, profissao, canal):
+    dados = {"profission": profissao, "channel": canal}
     return render_template('index.html', name=nome, data=dados)
+
 
 @app.route('/contato')
 def contato():
     return render_template('contato.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/autenticar', methods=['GET'])
+def autenticar():
+    usuario = request.args.get('user')
+    senha = request.args.get('pwd')
+
+    return "usuario: {} e senha: {}".format(usuario, senha)
